cc8531979a8bdefe69262638af0464d7d4f6b0a5
Mikkel Silvasen <mikkelkp360@gmail.com>
2025-04-07 09:17:41 +0200

    Changed from scoped to singleton databaseconnector

--- InventoryManagementBackend/Program.cs
+++ InventoryManagementBackend/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IDatabaseConnector, DatabaseConnector>();
+builder.Services.AddSingleton<IDatabaseConnector, DatabaseConnector>();
 
 var app = builder.Build();
 
